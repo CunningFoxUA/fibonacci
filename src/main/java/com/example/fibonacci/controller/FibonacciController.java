1a2e1f8b86028abc85c452beaca9045b622d8384
Roman Romaniv <romanromanivcom@gmail.com>
2025-12-01 18:10:39 +0200

    added Fibonacci

--- src/main/java/com/example/fibonacci/controller/FibonacciController.java
+++ src/main/java/com/example/fibonacci/controller/FibonacciController.java
@@ -5,6 +5,10 @@ import com.example.fibonacci.dto.StudentName;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/math")
 public class FibonacciController {
@@ -18,7 +22,32 @@ public class FibonacciController {
             @RequestParam FibonacciType type,
             @RequestParam Double input) {
         //write your code here;
+        if (student == StudentName.ROMAN) return RomanFib(input.intValue(), type);
 
         return ResponseEntity.ok("Fibonacci");
+    }
+
+    private ResponseEntity<String> RomanFib(int input, FibonacciType type){
+        List<BigInteger> nums = new ArrayList<>();
+        nums.add(BigInteger.ZERO); nums.add(BigInteger.ONE);
+
+        RomanRecursion(input, nums, type);
+
+        return ResponseEntity.ok(nums.toString());
+    }
+
+    private void RomanRecursion(int input, List<BigInteger> nums, FibonacciType type){
+        if (type == FibonacciType.STEP && input <= 0) return;
+
+        BigInteger last = nums.getLast();
+        if (type == FibonacciType.LIMIT && input <= last.intValue()) return;
+
+        BigInteger previous = nums.get(nums.size() - 2);
+
+        BigInteger next = last.add(previous);
+        nums.add(next);
+
+        if (type == FibonacciType.STEP)  RomanRecursion(input - 1, nums, type);
+        if (type == FibonacciType.LIMIT) RomanRecursion(input, nums, type);
     }
 }
