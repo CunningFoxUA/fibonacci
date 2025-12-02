c1d07fa1d772ec4bb3679c2410961a2abf5ee786
ShenheraOleksandr1 <oleksandrshengera@gmail.com>
2025-12-02 23:02:26 +0200

    added controller fibonacci-oleksandr

--- src/main/java/com/example/fibonacci/controller/FibonacciController.java
+++ src/main/java/com/example/fibonacci/controller/FibonacciController.java
@@ -5,6 +5,9 @@ import com.example.fibonacci.dto.StudentName;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigInteger;
+import java.util.*; // Map
+
 @RestController
 @RequestMapping("/math")
 public class FibonacciController {
@@ -13,12 +16,66 @@ public class FibonacciController {
     // і хочу побачити що викликається саме метод який написав Роман ,
     // це метод саме який рахує по кроках ну і інпет для нього 19
     @GetMapping("/{student}/fibonacci")
-    public ResponseEntity<String> fibonacci(
+    public ResponseEntity<Map<String, BigInteger>> fibonacci(
             @PathVariable StudentName student,
             @RequestParam FibonacciType type,
             @RequestParam Double input) {
         //write your code here;
+        if ((student == StudentName.OLEKSANDR) &&
+                (type == FibonacciType.STEP || type == FibonacciType.LIMIT))
+        {
+            return fibonacci(type, input);
+        }
 
-        return ResponseEntity.ok("Fibonacci");
+        return null;
     }
+
+    private ResponseEntity<Map<String, BigInteger>> fibonacci(FibonacciType type, Double input)
+    {
+        ArrayList<BigInteger> fibonacciArray = new ArrayList<>();
+
+        if (input <= 0.0) {
+            fibonacciArray.add(BigInteger.ZERO);
+            Map<String, BigInteger> fibonacciResponse = fibonacciJson(fibonacciArray);
+            return ResponseEntity.ok(fibonacciResponse);}
+
+        if (input == 1.0 || input == 2.0) {
+            fibonacciArray.add(BigInteger.ONE);
+            Map<String, BigInteger> fibonacciResponse = fibonacciJson(fibonacciArray);
+            return ResponseEntity.ok(fibonacciResponse);}
+
+        fibonacciArray.add(BigInteger.ZERO);
+        fibonacciArray.add(BigInteger.ONE);
+
+        fibonacciRecursion(fibonacciArray, type, input);
+
+        Map<String, BigInteger> fibonacciResponse = fibonacciJson(fibonacciArray);
+
+        return ResponseEntity.ok(fibonacciResponse);
+    }
+
+    private void fibonacciRecursion(ArrayList<BigInteger> array, FibonacciType type, Double input)
+    {
+        if (type == FibonacciType.STEP && input <= 0) return;
+
+        BigInteger next = array.get(array.size() - 2).add(array.getLast());
+        if (type == FibonacciType.LIMIT && input <= next.intValue()) return;
+
+        array.add(next);
+
+        if (type == FibonacciType.STEP) fibonacciRecursion(array, type, input - 1);
+        if  (type == FibonacciType.LIMIT) fibonacciRecursion(array, type, input);
+
+    }
+
+    private Map<String, BigInteger> fibonacciJson(ArrayList<BigInteger> array)
+    {
+        Map<String, BigInteger> response = new LinkedHashMap<>();
+        for (int i = 0; i <= array.size() - 1; i++)
+        {
+            response.put("step[" + i + "]",  array.get(i));
+        }
+        return response;
+    }
+
 }
